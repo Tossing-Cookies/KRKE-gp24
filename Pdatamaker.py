a1bd45c4b751303a8316498fbf0002b5e2b7cb55
Pepe066 <pietrotisci01@gmail.com>
2024-07-17 17:24:26 +0200

    aaaaaaaaa

---
+++ Pdatamaker.py
@@ -0,0 +1,12 @@
+youth_subcultures = [
+
+    {
+        "name": "Goths",
+        "valid_perspectives": ["Dark and mysterious"],
+        "location": "Bologna",
+        "valid_stereotypes": ["Morbid", "Mysterious"]
+        "ritual": [],
+        "style" : [],
+    }
+
+]
